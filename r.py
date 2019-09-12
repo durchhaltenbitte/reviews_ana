c198bc2ab8bda85ab198c6bbdc27cd15599ef270
durchhaltenbitte <shuang.ind@gmail.com>
2019-09-12 12:30:32 +0200

    add filting function

--- r.py
+++ r.py
@@ -10,6 +10,12 @@ with open('reviews.txt', 'r') as f:
 print('文件读取结束，共',len(data), '笔留言。')
 #算留言的平均长度
 sum_len = 0
+new = []
 for d in data:
+    if len(d) < 100:
+        new.append(d)
     sum_len += len(d)
 print('留言平均长度为:', sum_len/len(data))
+print('其中长度小于100的留言有', len(new), '笔。')
+
+
