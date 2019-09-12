6cf855a738fc856ead02e577c290259a451e71e2
durchhaltenbitte <shuang.ind@gmail.com>
2019-09-12 11:51:52 +0200

    calc avg length

--- r.py
+++ r.py
@@ -1,8 +1,15 @@
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line)
-		count += 1
-		if count % 10000 == 0:
-		    print(len(data))
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 10000 == 0:
+            print(len(data))
+
+print('文件读取结束，共',len(data), '笔留言。')
+#算留言的平均长度
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+print('留言平均长度为:', sum_len/len(data))
